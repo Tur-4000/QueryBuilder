eaa6befc73de07301cbdd39fcb730fc6da30cb3a
Turbanov Valeriy <tur.4000@gmail.com>
2019-09-23 17:08:42 +0300

    setUp tests

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -7,5 +7,27 @@ use QueryBuilder\Query;
 
 class QueryTest extends TestCase
 {
+    private $pdo;
 
+    public function setUp(): void
+    {
+        $articleTbl = "CREATE TABLE `articles` (
+                    `id`   INTEGER  NOT NULL
+                                  PRIMARY KEY AUTOINCREMENT,
+                    `name` VARCHAR  NOT NULL,
+                    `body` TEXT     NOT NULL
+                )";
+        $articles = "INSERT INTO `articles` (`name`, `body` )
+                     VALUES ('title1', 'body1'),
+                            ('title2', 'body2'),
+                            ('title3', 'body3')";
+        $this->pdo = new \PDO("sqlite::memory:");
+        $this->pdo->exec($articleTbl);
+        $this->pdo->exec($articles);
+    }
+
+    public function testSelect()
+    {
+
+    }
 }
